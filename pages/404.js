bc2df49934bff3d9017f2f811c38f09dc57c877e
Aayush <aayush.130702@gmail.com>
2022-11-29 17:17:52 +0530

    Updated

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,13 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 const Notfound = () => {
+    const router  = useRouter();
+    useEffect(()=>{
+        setTimeout(()=>{
+            router.push("/");
+        },3000)
+    },[])
     return ( 
         <div className="not-found text-center">
             <h1 className="bold text-5xl">Ooooops...</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,16 @@ import Footer from '../components/Footer'
 import Link from 'next/link'
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Homepage</title>
+    </Head>
     <div className={styles.title}>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quod corrupti libero error unde amet qui nihil modi sed expedita enim aspernatur, mollitia architecto doloribus non, cum magnam delectus possimus?</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis consequatur itaque id cum reiciendis impedit autem distinctio. Id ipsam velit iste obcaecati mollitia qui quo dolores possimus ab sapiente?</p>
-      <Link href="/lists" className={styles.btn}>See Ninjas List</Link>
-    </div>
+    <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quod corrupti libero error unde amet qui nihil modi sed expedita enim aspernatur, mollitia architecto doloribus non, cum magnam delectus possimus?</p>
+    <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis consequatur itaque id cum reiciendis impedit autem distinctio. Id ipsam velit iste obcaecati mollitia qui quo dolores possimus ab sapiente?</p>
+    <Link href="/lists" className={styles.btn}>See Ninjas List</Link>
+  </div>
+  </>
+    
   )
 }
