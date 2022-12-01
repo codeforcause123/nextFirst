2fcf443c22ff2f92240e003c9119d66fd47457f3
Aayush <aayush.130702@gmail.com>
2022-12-01 17:48:33 +0530

    Updated

--- pages/lists/[id].js
+++ pages/lists/[id].js
@@ -25,7 +25,10 @@ export const getStaticProps = async (context) => {
 const Details = ({ ninja }) => {
   return (
     <div className="flex items-center justify-center">
-      <Link href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <Link
+        href="#"
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {ninja.name}
         </h5>
