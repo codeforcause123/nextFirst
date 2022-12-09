f6ed88f39f516f236439a38b7f309fdd8c38eb09
Aayush <aayush.130702@gmail.com>
2022-12-09 17:46:49 +0530

    Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ export default function Home() {
       <title>Homepage</title>
     </Head>
     <div className={styles.title}>
-    <p className="text-2xl m-3 p-3 text-amber-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quod corrupti libero error unde amet qui nihil modi sed expedita enim aspernatur, mollitia architecto doloribus non, cum magnam delectus possimus?</p>
-    <p className="text-2xl m-3 p-3 text-pink-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis consequatur itaque id cum reiciendis impedit autem distinctio. Id ipsam velit iste obcaecati mollitia qui quo dolores possimus ab sapiente?</p>
+    <p className="text-2xl m-3 p-3 text-rose-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quod corrupti libero error unde amet qui nihil modi sed expedita enim aspernatur, mollitia architecto doloribus non, cum magnam delectus possimus?</p>
+    <p className="text-2xl m-3 p-3 text-purple-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis consequatur itaque id cum reiciendis impedit autem distinctio. Id ipsam velit iste obcaecati mollitia qui quo dolores possimus ab sapiente?</p>
     <Link href="/lists" className={styles.btn}>See Ninjas List</Link>
   </div>
   </>
